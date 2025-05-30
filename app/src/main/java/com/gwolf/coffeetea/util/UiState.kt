f75b19606e7cd9df7db24b89de7cc3bdc331c141
Artem Naumov <artem.naumov.dev@gmail.com>
2025-05-31 00:36:51 +0300

    -Added icon -Set icon to SplashScreen

--- app/src/main/java/com/gwolf/coffeetea/util/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.gwolf.coffeetea.util
-
-sealed class UiState<out T : Any?> {
-    data object Loading : UiState<Nothing>()
-    data class Success<out T : Any>(val data: T) : UiState<T>()
-    data class Error(val message: String?) : UiState<Nothing>()
-}
